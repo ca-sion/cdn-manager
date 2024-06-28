baade0d67da3830c098e9783bad3bda2be0f4366
michaelravedoni <michael@ravedoni.com>
2024-06-28 20:22:08 +0200

    up: payed status icon

--- app/Livewire/FrontListClients.php
+++ app/Livewire/FrontListClients.php
@@ -126,7 +126,7 @@ class FrontListClients extends Component implements HasForms, HasTable
                         $record->invoices?->where('id', $state)->first()?->number.' ('.
                         \Carbon\Carbon::parse($record->invoices?->where('id', $state)->first()?->date)->locale('fr_CH')->isoFormat('L').')'
                         .'</a>'.
-                        ($record->invoices?->where('id', $state)->first()?->status == 'payed' ? '✓' : null)
+                        ($record->invoices?->where('id', $state)->first()?->status->value == 'payed' ? ' ✓' : null)
                     ))
                     ->verticallyAlignStart(),
                 TextColumn::make('note')
