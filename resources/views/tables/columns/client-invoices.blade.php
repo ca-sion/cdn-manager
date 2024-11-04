0ae4b15444ba927f306c3bbda072151c6a600543
michaelravedoni <michael@ravedoni.com>
2024-11-04 13:27:59 +0100

    up: ui invoices

--- resources/views/tables/columns/client-invoices.blade.php
+++ resources/views/tables/columns/client-invoices.blade.php
@@ -5,11 +5,15 @@
     <table class="my-2">
         @foreach ($record->invoices as $invoice)
         <div>
-            <a href="{{ $invoice->link }}" target="_blank">📄</a>
+            <a href="{{ $invoice->link }}" target="_blank">
+                <x-heroicon-o-document class="inline h-3" />
+            </a>
             <a href="/admin/invoices/{{ $invoice->id }}/edit">{{ $invoice->number }}</a>
-            ({{ $invoice->status->getLabel() }})
+            <span style="--c-50:var(--{{ $invoice->status->getColor() }}-50);--c-400:var(--{{ $invoice->status->getColor() }}-400);--c-600:var(--{{ $invoice->status->getColor() }}-600);" class="inline-flex fi-badge flex items-center justify-center gap-x-1 rounded-md text-xs font-medium ring-1 ring-inset px-2 min-w-[theme(spacing.6)] py-1 fi-color-custom bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30 fi-color-{{ $invoice->status->getColor() }}">
+                {{ $invoice->status->getLabel() }}
+            </span>
             @if ($invoice->paid_on)
-                ✅
+            <x-heroicon-o-check-circle class="inline h-5" style="color: green;" />
             @endif
         </div>
         @endforeach
