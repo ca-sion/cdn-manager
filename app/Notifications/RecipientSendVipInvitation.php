94c40416606c63b57aca029294233af72c3c02f2
michaelravedoni <michael@ravedoni.com>
2024-11-18 12:56:21 +0100

    up: vip text

--- app/Notifications/RecipientSendVipInvitation.php
+++ app/Notifications/RecipientSendVipInvitation.php
@@ -46,13 +46,13 @@ class RecipientSendVipInvitation extends Notification
             // ->bcc('info@coursedenoel.ch')
             ->greeting(new HtmlString('<small>Pour '.$notifiable->name.',</small><br>Chère Amie, Cher Ami,'))
             ->line('Le **14 décembre prochain**, la Place de la Planta et les rues de la vieille ville de Sion vibreront à nouveau sous les applaudissements du public venu encourager les participants de la Course de Noël et du Trail des Châteaux.')
-            ->line('Comme le veut la tradition, nous vous invitons dans notre espace VIP Swiss Life sur la Place de la Planta pour un apéritif-raclette qui vous sera servi dès 16h30 selon le programme décrit ci-après. Les festivités se poursuivront sous la tente des fêtes.')
+            ->line('Comme le veut la tradition, nous vous invitons dans l’espace VIP Swiss Life sur la Place de la Planta pour un apéritif-raclette qui vous sera servi dès 16h30 selon le programme décrit ci-après. Les festivités se poursuivront sous la tente des fêtes.')
             ->line('📨 [Invitation et programme](https://coursedenoel.ch/assets/documents/2024-invitation-vip-simple.pdf)')
-            ->lineIf($this->provisionElement->vip_invitation_number > 1, '🔢 Nombre d\'invitations : '.$this->provisionElement->vip_invitation_number)
+            ->lineIf($this->provisionElement->vip_invitation_number > 1, '🔢 Nombre d’invitations : '.$this->provisionElement->vip_invitation_number)
             ->line('Inscription souhaitée jusqu’au **1er décembre 2024** en remplissant le formulaire ci-après :')
-            ->action('✍️ Répondre à l\'invitation (oui/non)', $responseUrl)
-            ->line('Vous trouverez plus d’infos sur nos deux courses sur [coursedenoel.ch](https://coursedenoel.ch) et [traildeschateaux.ch](https://traildeschateaux.ch).')
-            ->line('Nous nous réjouissons de vous accueillir dans notre espace VIP Swiss Life et vous présentons, Mesdames, Messieurs, Chères Amies, Chers Amis, nos salutations sportives.')
+            ->action('✍️ Répondre à l’invitation (oui/non)', $responseUrl)
+            ->line('Vous trouverez plus d’informations sur nos deux courses sur [coursedenoel.ch](https://coursedenoel.ch) et [traildeschateaux.ch](https://traildeschateaux.ch).')
+            ->line('Nous nous réjouissons de vous accueillir dans l’espace VIP Swiss Life et vous présentons, Mesdames, Messieurs, Chères Amies, Chers Amis, nos salutations sportives.')
             ->salutation(new HtmlString('Dominique Solioz<br>Président du Co de la Course de Noël<br><br>David Valterio<br>Président du Co du Trail des Châteaux'));
 
         if ($this->provisionElement->recipientVipContactEmail && $this->provisionElement->recipientContactEmail && $this->provisionElement->recipientVipContactEmail != $this->provisionElement->recipientContactEmail) {
