b55edbd5a1e8c88fbcc50d9c71cfb72c2452aed7
michaelravedoni <michael@ravedoni.com>
2024-11-26 14:14:54 +0100

    fix: date

--- app/Livewire/FrontListProvisions.php
+++ app/Livewire/FrontListProvisions.php
@@ -133,7 +133,7 @@ class FrontListProvisions extends Component implements HasForms, HasTable
                     ->visible($this->isFieldInUrl('contact_location')),
                 TextColumn::make('contact_date')
                     ->label('Date')
-                    ->date('L', 'fr_CH')
+                    ->date('d.m.Y')
                     ->visible($this->isFieldInUrl('contact_date')),
                 TextColumn::make('contact_time')
                     ->label('Heure')
