ad8193789e0e5b0def042ad4835e605cf78f631e
Michael Ravedoni <30499597+michaelravedoni@users.noreply.github.com>
2025-10-28 10:39:23 +0100

    up: districts

--- app/Notifications/ContactDonorFormLink.php
+++ app/Notifications/ContactDonorFormLink.php
@@ -46,7 +46,7 @@ class ContactDonorFormLink extends Notification
             ->bcc('pub@coursedenoel.ch')
             ->greeting('Cher Ami de la Course de Noël et du Trail des Châteaux,')
             ->line('Comme vous le savez, la Course de Noël est devenue un événement incontournable du 2ᵉ samedi de décembre à Sion. Cette année, nous organisons la 56ᵉ édition et nous sommes impatients d’accueillir plus de 6000 participant·e·s le 13 décembre 2025. Le 7ᵉ Trail des Châteaux se tiendra le même jour entre les châteaux de notre belle région. Votre contribution, même modeste, est précieuse. Elle nous permettra de continuer à offrir un événement de qualité.')
-            ->line('En remerciement de votre générosité, votre nom sera mentionné dans l\'encarté du Nouvelliste, distribué à 40 000 exemplaires dans le district de Conthey le 2 décembre. C\'est une belle opportunité de montrer votre engagement auprès de la communauté. Mention anonyme possible.')
+            ->line('En remerciement de votre générosité, votre nom sera mentionné dans l\'encarté du Nouvelliste, distribué à 40 000 exemplaires dans les districts de Sion, d’Hérens et de Conthey le 2 décembre. C\'est une belle opportunité de montrer votre engagement auprès de la communauté. Mention anonyme possible.')
             ->line('Si vous souhaitez **soutenir** la Course de Noël et le Trail des Châteaux, nous vous invitons à **remplir le formulaire en ligne** suivant :')
             ->action('Formulaire de don', $signedUrl)
             ->line('Nous restons à disposition en cas de questions ou pour tout complément d\'information.')
