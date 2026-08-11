dedb1aedf60cab68c5a4a1f065e867a6cf7803dd
Michael Ravedoni <30499597+michaelravedoni@users.noreply.github.com>
2026-08-11 16:10:03 +0200

    fix: style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Course de Noël' }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @livewireStyles
+    @filamentStyles
+    @vite(['resources/css/app.css'])
 </head>
 <body class="bg-gray-100 font-sans antialiased">
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         {{ $slot }}
     </div>
-    @livewireScripts
+    @livewire('notifications')
+    @filamentScripts
+    @vite('resources/js/app.js')
 </body>
 </html>
